80682e3c6261fc0de78db6ee78dc71a981c8f953
Frederic Dlugi <frederic.dlugi@student.uni-luebeck.de>
2021-03-11 22:09:31 +0100

    workaround for ant being a pile of fail

---
+++ face_finder.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNetFromCaffe("bvlc_googlenet.prototxt", "res10_300x300_ssd_iter_140000.caffemodel")
+
+def find_faces(img, net, confidence=0.7):
+    (h, w) = img.shape[:2]
+    mean = np.mean(img,axis=(0,1))
+    resized_img = cv2.resize(img, (300, 300))
+    blob = cv2.dnn.blobFromImage(resized_img, 1.0,
+        (300, 300), mean)
+
+    faces = []
+
+    net.setInput(blob)
+    detections = net.forward()
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        conf = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if conf < confidence:
+            continue
+        # compute the (x, y)-coordinates of the bounding box for the
+        # object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+        faces.append((conf, startX, startY, endX, endY))
+    return faces
+
+def draw_face_box(img, faces):
+    for i in range(len(faces)):
+        (conf, startX, startY, endX, endY) = faces[i]
+        # draw the bounding box of the face along with the associated
+        # probability
+        text = f"{conf * 100:.2f}%"
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(img, (startX, startY), (endX, endY),
+            (0, 0, 255), 2)
+        cv2.putText(img, text, (startX, y),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
--- grab_thread.py
+++ grab_thread.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import cv2
+from face_finder import find_faces, net, draw_face_box
 
 class GrabThread(QObject):
 
@@ -63,7 +64,7 @@ class GrabThread(QObject):
     def run(self):
         print("started GrabThread")
         self.camera.MaxNumBuffer = 20
-        self.camera.StartGrabbingMax(100000000, pylon.GrabStrategy_LatestImages)
+        self.camera.StartGrabbingMax(1_000_000_000, pylon.GrabStrategy_LatestImages)
         i = 0
         while self.running:
 
@@ -83,13 +84,24 @@ class GrabThread(QObject):
                 else:
                     if self.is_color:
                         img = cv2.cvtColor(self.frame, cv2.COLOR_YUV2BGR_YUY2)
+                        if len(self.faces) >= 1:
+                            draw_face_box(img, self.faces)
+
                     else:
                         img = self.frame
                     cv2.imshow("Preview", img)
                     cv2.waitKey(1)
 
             if i % 10 == 0:
+                img = cv2.cvtColor(self.frame, cv2.COLOR_YUV2BGR_YUY2)
+                faces = set_auto_functions(self.camera, img)
+                if len(faces) >= 1:
+                    self.faces = faces
+
                 self.avg_fps.emit(self.virt_cam._fps_counter.avg_fps)
+
+            if len(self.faces) >= 1:
+                center_face(self.camera, self.faces[0])
 
             self.virt_cam.sleep_until_next_frame()
             i += 1
@@ -105,3 +117,70 @@ class GrabThread(QObject):
         print("finished GrabThread")
         self.finished.emit()
         self.avg_fps.emit(0)
+
+def set_auto_functions(camera, img):
+    faces = find_faces(img, net)
+    offX = camera.OffsetX.GetValue()
+    offY = camera.OffsetY.GetValue()
+
+    if len(faces) >= 1:
+        (conf, startX, startY, endX, endY) = faces[0]
+
+        face_width =  endX - startX
+        face_height = endY - startY
+        face_abs_start_x = offX + startX
+        face_abs_start_y = offY + startY
+
+
+        set_int_value(camera.AutoFunctionROIWidth, face_width)
+        set_int_value(camera.AutoFunctionROIHeight, face_height)
+        set_int_value(camera.AutoFunctionROIOffsetX, face_abs_start_x)
+        set_int_value(camera.AutoFunctionROIOffsetY, face_abs_start_y)
+
+    return faces
+
+def center_face(camera, face):
+    dead_zone = 200
+    incX = camera.OffsetX.Inc
+    incY = camera.OffsetX.Inc
+    offX = camera.OffsetX.GetValue()
+    offY = camera.OffsetY.GetValue()
+
+
+    (conf, startX, startY, endX, endY) = face
+
+    face_width =  endX - startX
+    face_height = endY - startY
+
+    face_center_x = startX + face_width // 2
+    face_center_y = startY + face_height // 2
+
+    height = camera.Height.Value
+    width = camera.Width.Value
+
+    if face_center_x > ((width/2)+dead_zone):
+        new_off_x = offX + incX
+    elif face_center_x < ((width/2)-dead_zone):
+        new_off_x = offX - incX
+    else:
+        new_off_x = offX
+
+
+    if face_center_y < ((height/2)- dead_zone):
+        new_off_y = offY - incY
+    elif face_center_y > ((height/2) + dead_zone):
+        new_off_y = offY + incY
+    else:
+        new_off_y = offY
+
+    set_int_value(camera.OffsetX, new_off_x)
+    set_int_value(camera.OffsetY, new_off_y)
+
+
+def set_int_value(feature, value):
+    val_0_min = value - feature.Min
+    val_corr_inc = (val_0_min // feature.Inc) * feature.Inc
+
+    val = min(max(val_corr_inc + feature.Min, feature.Min), feature.Max)
+
+    feature.Value = int(val)
